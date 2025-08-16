d4279302c8dae349467ba58ad6f395f052deeb14
Lee jeongchan <sjk338285@gmail.com>
2025-08-16 20:37:54 +0900

    chore : typeORM 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,27 @@
-import { Module } from '@nestjs/common';
+import { Module } from "@nestjs/common";
 import { join } from "node:path";
 import { ConfigModule, ConfigService } from "@nestjs/config";
 import { TypeOrmModule } from "@nestjs/typeorm";
-import { typeormDataSourceFactory, typeormOptionsFactory } from "./config/typeorm";
+import {
+  typeormDataSourceFactory,
+  typeormOptionsFactory,
+} from "./config/typeorm";
 import { RedisModule } from "./config/redis";
-import { HealthModule } from './health/health.module';
-import { PrismaModule } from './prisma/prisma.module';
+import { HealthModule } from "./health/health.module";
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: join(__dirname, "..", ".env")
+      envFilePath: join(__dirname, "..", ".env"),
     }),
     TypeOrmModule.forRootAsync({
       useFactory: typeormOptionsFactory,
       dataSourceFactory: typeormDataSourceFactory,
-      inject: [ConfigService]
+      inject: [ConfigService],
     }),
     RedisModule,
     HealthModule,
-    PrismaModule,
   ],
   providers: [],
   controllers: [],

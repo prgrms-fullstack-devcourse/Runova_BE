dbe5c169d4ae8f679702c3dcd93a017a7b39dae4
Lee jeongchan <sjk338285@gmail.com>
2025-08-17 17:54:50 +0900

    feat : jwt strategy, guard 구현

---
+++ src/auth/strategies/access-jwt.strategy.ts
@@ -0,0 +1,21 @@
+// src/auth/strategies/access-jwt.strategy.ts
+import { Injectable } from "@nestjs/common";
+import { PassportStrategy } from "@nestjs/passport";
+import { ExtractJwt, Strategy } from "passport-jwt";
+
+@Injectable()
+export class AccessJwtStrategy extends PassportStrategy(Strategy, "jwt") {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.JWT_ACCESS_SECRET,
+    });
+  }
+
+  async validate(payload: any) {
+    return {
+      userId: payload.sub,
+      nickname: payload.nickname,
+    };
+  }
+}

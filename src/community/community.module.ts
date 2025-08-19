e8582db9a765714b7ad6a66d37dc70265b9c1c1d
Lee jeongchan <sjk338285@gmail.com>
2025-08-19 16:20:34 +0900

    DI 해결

--- src/community/community.module.ts
+++ src/community/community.module.ts
@@ -1,9 +1,14 @@
-import { Module } from '@nestjs/common';
-import { CommunityController } from './community.controller';
-import { CommunityService } from './community.service';
+import { Module } from "@nestjs/common";
+import { CommunityController } from "./community.controller";
+import { CommunityService } from "./community.service";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Comment } from "src/modules/posts/comment.entity";
+import { PostLike } from "src/modules/posts/post-like.entity";
+import { Post } from "src/modules/posts/post.entity";
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Post, Comment, PostLike])],
   controllers: [CommunityController],
-  providers: [CommunityService]
+  providers: [CommunityService],
 })
 export class CommunityModule {}

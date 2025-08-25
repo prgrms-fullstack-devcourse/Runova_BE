a1625298be53f8764f82e9b649cb64b03a4d962a
Lee jeongchan <sjk338285@gmail.com>
2025-08-25 19:03:04 +0900

    feat : 의존성 정리

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,17 +6,15 @@ import { TokensService } from "./tokens.service";
 import { TypeOrmModule } from "@nestjs/typeorm";
 
 import { User } from "../modules/users/user.entity";
-import { SocialAccount } from "../modules/auth/social-account.entity";
-import { UserSession } from "../modules/auth/user-session.entity";
 import { AccessJwtStrategy } from "./strategies/access-jwt.strategy";
 import { JwtModule } from "@nestjs/jwt";
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([User, SocialAccount, UserSession]),
+    TypeOrmModule.forFeature([User]),
     JwtModule.register({
-      secret: process.env.JWT_ACCESS_SECRET, // 또는 직접 문자열
-      signOptions: { expiresIn: process.env.JWT_ACCESS_EXPIRES_IN }, // 기본 옵션
+      secret: process.env.JWT_ACCESS_SECRET,
+      signOptions: { expiresIn: process.env.JWT_ACCESS_EXPIRES_IN },
     }),
   ],
   controllers: [AuthController],

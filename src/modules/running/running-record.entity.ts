14c09b675ef067cf7be87c356d105835efb018f7
Lee jeongchan <sjk338285@gmail.com>
2025-08-25 16:07:27 +0900

    fix : datatime ->timestamptz 로 변경

--- src/modules/running/running-record.entity.ts
+++ src/modules/running/running-record.entity.ts
@@ -1,5 +1,12 @@
 import { ImmutableEntityBase } from "../../common/entity";
-import { Column, Entity, Index, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import {
+  Column,
+  Entity,
+  Index,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from "typeorm";
 import { User } from "../users";
 import { Course } from "../courses";
 import transformer from "../../utils/datetime-transformer";
@@ -8,40 +15,40 @@ import { Coordinates, LineStringColumn } from "../../common/geo";
 
 @Entity("running_records")
 export class RunningRecord extends ImmutableEntityBase {
-    @PrimaryGeneratedColumn()
-    id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Index()
-    @Column({ name: "user_id", type: "int" })
-    userId: number;
+  @Index()
+  @Column({ name: "user_id", type: "int" })
+  userId: number;
 
-    @ManyToOne(() => User, { onDelete: "CASCADE" })
-    @JoinColumn({ name: "user_id" })
-    user: User;
+  @ManyToOne(() => User, { onDelete: "CASCADE" })
+  @JoinColumn({ name: "user_id" })
+  user: User;
 
-    @Index()
-    @Column({ name: "course_id", type: "int", nullable: true })
-    courseId: number | null;
+  @Index()
+  @Column({ name: "course_id", type: "int", nullable: true })
+  courseId: number | null;
 
-    @ManyToOne(() => Course, { onDelete: "SET NULL" })
-    @JoinColumn({ name: "course_id" })
-    course: Course | null;
+  @ManyToOne(() => Course, { onDelete: "SET NULL" })
+  @JoinColumn({ name: "course_id" })
+  course: Course | null;
 
-    @LineStringColumn()
-    path: Coordinates[];
+  @LineStringColumn()
+  path: Coordinates[];
 
-    @Column({ name: "start_at", type: "datetime", transformer })
-    startAt: LocalDateTime;
+  @Column({ name: "start_at", type: "timestamptz", transformer })
+  startAt: LocalDateTime;
 
-    @Column({ name: "end_at", type: "datetime", transformer })
-    endAt: LocalDateTime;
+  @Column({ name: "end_at", type: "timestamptz", transformer })
+  endAt: LocalDateTime;
 
-    @Column({ type: "float8" })
-    distance: number;
+  @Column({ type: "float8" })
+  distance: number;
 
-    @Column({ type: "float8" })
-    pace: number;
+  @Column({ type: "float8" })
+  pace: number;
 
-    @Column({ type: "float8" })
-    calories: number;
+  @Column({ type: "float8" })
+  calories: number;
 }

2e8307cb44f3a51c7784583e439bedd71c4d6b79
hoseok <hs1j333@gmail.com>
2025-08-26 10:00:44 +0900

    fix/courses

--- src/config/proj4.ts
+++ src/config/proj4.ts
@@ -6,6 +6,5 @@ export const CRS_NAME: string = process.env.GIS_EPSG_NAME ?? "EPSG:5179";
 proj.defs(
     CRS_NAME,
     process.env.GIS_EPSG_PROJECTION
-    ?? "+proj=tmerc +lat_0=38 +lon_0=127.5 +k=0.9996 +x_0=1000000 +y_0=2000000 " +
-    "+ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"
+    ?? "+proj=tmerc +lat_0=38 +lon_0=127.5 +k=0.9996 +x_0=1000000 +y_0=2000000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"
 );

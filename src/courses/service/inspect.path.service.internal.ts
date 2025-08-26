40f239f660dde94c734ade9969ca01281b7363d5
hoseok <hs1j333@gmail.com>
2025-08-26 15:47:36 +0900

    refactor/2

--- src/courses/service/inspect.path.service.internal.ts
+++ src/courses/service/inspect.path.service.internal.ts
@@ -1,4 +1,4 @@
-import { Location, makeDisplacements } from "../../common/geo";
+import { Coordinates, Location } from "../../common/geo";
 import { CourseNodeDTO, InspectPathResult } from "../dto";
 import { globeToProjected } from "../../config/proj4";
 
@@ -6,20 +6,23 @@ export function inspectPath(
     path: Location[],
 ): InspectPathResult {
     const line = path.map(globeToProjected);
-    const displacements = makeDisplacements(line);
+    const segments = __makeSegments(line);
 
     let length = 0;
+    let prevSeg: [number, number] = [0, 0];
     const nodes: CourseNodeDTO[] = [];
 
-    displacements.forEach((r, i) => {
+
+    segments.forEach((seg, i) => {
 
         nodes.push({
             coordinates: line[i],
             progress: length,
-            bearing: r.bearing()
+            bearing: __bearing(prevSeg, seg)
         });
 
-        length += r.length();
+        length += __length(seg);
+        prevSeg = seg;
     });
 
     nodes.push({
@@ -30,6 +33,45 @@ export function inspectPath(
 
    return { length, nodes };
 }
+
+
+function __makeSegments(line: Coordinates[]): [number, number][] {
+    const results: [number, number][] = [];
+
+    for (let i = 0; i !== line.length - 1; ++i) {
+        const { x: x1, y: y1 } = line[i];
+        const { x: x2, y: y2 } = line[i + 1];
+        results.push([x2 - x1, y2 - y1]);
+    }
+
+    return results;
+}
+
+/**
+ *
+ * return length of segment in km
+ */
+function __length(seg: [number, number]): number {
+    return Math.sqrt(
+        seg.reduce(
+            (acc, curr) => acc + curr * curr,
+            0
+        )
+    ) / 1000;
+}
+
+/**
+ *
+ * return bearing angle between two segments
+ */
+function __bearing(
+    [x1, y1]: [number, number],
+    [x2, y2]: [number, number]
+): number {
+    return (Math.atan2(x2 - x1, y2 - y1) * 180) / Math.PI;
+}
+
+
 
 
 

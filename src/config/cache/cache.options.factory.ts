a4d1ae4f32e1c3b09a2a75ac6a389f419b135b4d
hoseok <hs1j333@gmail.com>
2025-08-27 10:35:09 +0900

    cache module 설정

---
+++ src/config/cache/cache.options.factory.ts
@@ -0,0 +1,9 @@
+import Redis from "iovalkey";
+import Keyv from "keyv";
+import KeyvValkey from "@keyv/valkey";
+import { CacheOptions } from "@nestjs/cache-manager";
+
+export function cacheOptionsFactory(redis: Redis): CacheOptions {
+    const store = new KeyvValkey(redis);
+    return { stores: [new Keyv({ store })] };
+}

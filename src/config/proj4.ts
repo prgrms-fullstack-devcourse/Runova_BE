9d9bc2727240b9e304f01a8277ec867be857ec50
hoseok <hs1j333@gmail.com>
2025-08-28 14:58:04 +0900

    Revert "fix/courses"
    This reverts commit b46ba16285948a6b566a3ef9534e4e0ebf539e75.

--- src/config/proj4.ts
+++ src/config/proj4.ts
@@ -1,11 +1,8 @@
 import proj from "proj4";
-import process from "node:process";
 
-export const CRS_NAME: string = process.env.GIS_EPSG_NAME ?? "EPSG:5179";
+const __WGS84 = "EPSG:4326";
+const __UTM_K = "EPSG:5179";
+const __PROJ = "+proj=tmerc +lat_0=38 +lon_0=127.5 +k=0.9996 +x_0=1000000 +y_0=2000000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs";
 
-proj.defs(
-    CRS_NAME,
-    process.env.GIS_EPSG_PROJECTION
-    ?? "+proj=tmerc +lat_0=38 +lon_0=127.5 +k=0.9996 +x_0=1000000 +y_0=2000000 " +
-    "+ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"
-);
+proj.defs(__UTM_K, __PROJ);
+export default proj(__WGS84, __UTM_K);

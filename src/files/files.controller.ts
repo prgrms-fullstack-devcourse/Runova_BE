ea12059d9a42d8cb2ba6ce038e7c3bf467230888
Lee jeongchan <sjk338285@gmail.com>
2025-09-01 18:11:16 +0900

    feat : url 발급 로직 구현

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from "@nestjs/common";
+import { FilesService } from "./files.service";
+import { PresignFileDto } from "./dto/presign-file.dto";
+import { User } from "src/utils/decorator/http.decorators";
 
-@Controller('files')
-export class FilesController {}
+@Controller("files")
+export class FilesController {
+  constructor(private readonly filesService: FilesService) {}
+
+  @Post("presign")
+  async presign(@User("userId") userId: number, @Body() dto: PresignFileDto) {
+    return this.filesService.getPresignedUrl(
+      dto.type,
+      dto.contentType,
+      dto.size,
+      userId
+    );
+  }
+}
--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -1,4 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { UploadType } from "../common/constants/upload-type.enum";
+import { randomUUID } from "crypto";
+import { fromEnv } from "@aws-sdk/credential-providers/dist-types/fromEnv";
+
+const S3_EXPIRES_IN = 300;
 
 @Injectable()
-export class FilesService {}
+export class FilesService {
+  private readonly s3 = new S3Client({
+    region: process.env.AWS_REGION!,
+    credentials: fromEnv(),
+  });
+
+  async getPresignedUrl(
+    type: UploadType,
+    contentType: string,
+    size: number,
+    userId: number
+  ) {
+    const bucket = process.env.S3_BUCKET!;
+    const fileName = `${randomUUID()}`;
+    const today = new Date().toISOString().split("T")[0].replace(/-/g, "");
+    const key =
+      type === UploadType.AVATAR
+        ? `users/${userId}/avatar/${fileName}`
+        : `users/${userId}/verify/${today}/${fileName}`;
+
+    const command = new PutObjectCommand({
+      Bucket: bucket,
+      Key: key,
+      ContentType: contentType,
+      ContentLength: size,
+      ACL: "private",
+    });
+
+    const url = await getSignedUrl(this.s3, command, {
+      expiresIn: S3_EXPIRES_IN,
+    });
+
+    return { url, key, bucket, expiresIn: S3_EXPIRES_IN };
+  }
+}

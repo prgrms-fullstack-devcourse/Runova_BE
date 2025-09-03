9740943a188622ec1e9c0a3be29f655e606793f1
hoseok <hs1j333@gmail.com>
2025-09-03 13:07:20 +0900

    fix

--- src/running/dto/running.record.filters.ts
+++ src/running/dto/running.record.filters.ts
@@ -1,5 +1,13 @@
-
 //--ToDo 필터 조건 확정하기
+import { ApiProperty } from "@nestjs/swagger";
+import { IsDate, IsOptional } from "class-validator";
+import { Transform } from "class-transformer";
+
 export class RunningRecordFilters {
 
+    @IsDate()
+    @IsOptional()
+    @Transform(({ value }) => value && new Date(value))
+    @ApiProperty({ type: Date })
+    startDate?: Date;
 }

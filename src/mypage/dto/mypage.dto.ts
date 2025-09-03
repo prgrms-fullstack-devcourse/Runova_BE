9f0f7a2c2b90626a4d8ec3830f510685d1f9fd36
Lee jeongchan <sjk338285@gmail.com>
2025-09-03 17:38:33 +0900

    feat : mypage dto 정의

---
+++ src/mypage/dto/mypage.dto.ts
@@ -0,0 +1,32 @@
+export class MyPageOverviewDto {
+  profile: {
+    id: number;
+    nickname: string;
+    email: string;
+    avatarUrl: string;
+    description: string;
+    createdAt: string;
+  };
+  myCourses: Array<{
+    id: number;
+    title: string;
+    length: number;
+    createdAt: string;
+    previewImageUrl: string;
+  }>;
+  myPosts: Array<{
+    id: number;
+    title: string;
+    content: string;
+    createdAt: string;
+    likeCount: number;
+    commentCount: number;
+  }>;
+  myPhotos: Array<{
+    id: number;
+    courseId: number;
+    title: string;
+    imageUrl: string;
+    createdAt: string;
+  }>;
+}

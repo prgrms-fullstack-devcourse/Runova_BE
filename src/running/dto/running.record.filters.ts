6250ae6a03edcef58529613affd17776434ea00e
hoseok <hs1j333@gmail.com>
2025-09-03 13:07:25 +0900

    Revert "fix"
    This reverts commit 153aa124e93f91e09b717db9b5b53fd2423f85a9.

--- src/running/dto/running.record.filters.ts
+++ src/running/dto/running.record.filters.ts
@@ -1,13 +1,5 @@
-//--ToDo 필터 조건 확정하기
-import { ApiProperty } from "@nestjs/swagger";
-import { IsDate, IsOptional } from "class-validator";
-import { Transform } from "class-transformer";
 
+//--ToDo 필터 조건 확정하기
 export class RunningRecordFilters {
 
-    @IsDate()
-    @IsOptional()
-    @Transform(({ value }) => value && new Date(value))
-    @ApiProperty({ type: Date })
-    startDate?: Date;
 }

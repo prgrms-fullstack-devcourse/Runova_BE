7a138117e79500a4bd44b440a36dafe72e3129f2
Lee jeongchan <sjk338285@gmail.com>
2025-09-08 15:44:53 +0900

    의존성 누락 해결

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,9 +3,11 @@ import { UserController } from "./user.controller";
 import { UserService } from "./user.service";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { User } from "src/modules/users/user.entity";
+import { Course } from "src/modules/courses";
+import { Post } from "../modules/posts/post.entity";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, Course, Post])],
   controllers: [UserController],
   providers: [UserService],
 })

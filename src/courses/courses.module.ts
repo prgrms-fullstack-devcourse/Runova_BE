dd01603c083684f7f26dff4b71a6d3e4e0792613
hoseok <hs1j333@gmail.com>
2025-09-13 14:07:36 +0900

    Revert "sentry 설정"
    This reverts commit ae1293162fb187dade0520b62711a916b8fe07f0.

--- src/courses/courses.module.ts
+++ src/courses/courses.module.ts
@@ -8,10 +8,6 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { Course, CourseBookmark, CourseNode } from "../modules/courses";
 import { CoursesController, SearchCoursesController } from "./controller";
 import { SearchCoursesInterceptor, SearchCoursesResponseInterceptor } from "./interceptor";
-import { WorkersPoolModule, WorkersPoolService } from "../config/workerpool";
-import { resolve } from "node:path";
-
-const __EXTERNAL_PROVIDERS = [WorkersPoolService];
 
 @Module({
   imports: [
@@ -20,12 +16,8 @@ const __EXTERNAL_PROVIDERS = [WorkersPoolService];
           CourseNode,
           CourseBookmark,
       ]),
-      WorkersPoolModule.register(
-          resolve(__dirname, "worker/inspect.path.worker.js")
-      ),
   ],
   providers: [
-      ...__EXTERNAL_PROVIDERS,
       CoursesService,
       CourseBookmarksService,
       SearchCoursesService,
--- src/courses/service/inspect.path.service.ts
+++ src/courses/service/inspect.path.service.ts
@@ -1,19 +1,34 @@
-import { Inject, Injectable } from "@nestjs/common";
+import { Inject, Injectable, OnModuleDestroy } from "@nestjs/common";
 import { InspectPathResult } from "../dto";
-import { WorkersPoolService } from "../../config/workerpool";
+import { ConfigService } from "@nestjs/config";
+import { Pool, WorkerPoolOptions, pool } from "workerpool";
+import { resolve } from "node:path";
+
 @Injectable()
-export class InspectPathService {
+export class InspectPathService implements OnModuleDestroy {
+    private readonly pool: Pool;
 
     constructor(
-       @Inject(WorkersPoolService)
-       private readonly poolService: WorkersPoolService,
-    ) {}
+       @Inject(ConfigService)
+       config: ConfigService,
+    ) {
+        const workerPath: string = resolve(__dirname, "../worker/inspect.path.worker.js");
+
+        const options: WorkerPoolOptions = {
+            workerType: "thread",
+            maxWorkers: config.get<number>("POOL_MAX_WORKERS") ?? 5,
+            maxQueueSize: config.get<number>("POOL_MAX_QUEUE_SIZE") ?? 30,
+        };
+
+        this.pool = pool(workerPath, options);
+    }
+
+    async onModuleDestroy(): Promise<void> {
+        await this.pool.terminate(true);
+    }
 
     async inspectPath(path: [number, number][]): Promise<InspectPathResult> {
-        return this.poolService.exec<InspectPathResult>(
-            "inspectPath",
-            path,
-        );
+        return await this.pool.exec("inspectPath", [path]);
     }
 }
 

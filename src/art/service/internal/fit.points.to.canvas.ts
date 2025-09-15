eda49a66ee56b63e96f9fe5cad45eedbad39a75c
hoseok <hs1j333@gmail.com>
2025-09-15 18:43:26 +0900

    image 생성 구현

--- src/art/service/internal/fit.points.to.canvas.ts
+++ src/art/service/internal/fit.points.to.canvas.ts
@@ -15,7 +15,7 @@ export function fitPointsToCanvas(
     (height - 2 * margin) / h,
   );
 
-  for (let i = 0; i !== points.length - 1; i += 2) {
+  for (let i = 0; i < points.length - 1; i += 2) {
     points[i] = (points[i] - xMin) * s + (width - s * w) * 0.5;
     points[i + 1] = (points[i + 1] - yMin) * s + (height - s * h) * 0.5;
   }
@@ -27,7 +27,7 @@ function __findBounds(points: Float32Array): [number, number, number, number] {
   let xMax = xMin;
   let yMax = yMin;
 
-  for (let i = 0; i !== points.length - 1; i += 2) {
+  for (let i = 0; i < points.length - 1; i += 2) {
     xMin = Math.min(xMin, points[i]);
     yMin = Math.min(yMin, points[i + 1]);
     xMax = Math.max(xMax, points[i]);
